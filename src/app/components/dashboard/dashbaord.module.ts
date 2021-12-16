61fcab0ab489c4ca6874c204199c32fe0846b0b8
VamanPatel <vamankumar456@gmail.com>
2021-12-16 23:06:49 +0530

    validation while adding notes and some ui changes

--- src/app/components/dashboard/dashbaord.module.ts
+++ src/app/components/dashboard/dashbaord.module.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { ColorPickerModule } from "ngx-color-picker";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { AuthService } from "src/app/Services/auth.service";
 import { MyProfileComponent } from "../my-profile/my-profile.component";
@@ -9,16 +13,20 @@ import { DashbaordRoutingModule } from "./dashboard-routing.module";
 import { DashboardComponent } from "./dashboard.component";
 
 @NgModule({
-    declarations: [DashboardComponent, MyProfileComponent],
-    imports: [
-        DashbaordRoutingModule,
-        ReactiveFormsModule,
-        NgxSpinnerModule,
-        CommonModule,
-        FormsModule,
-        MatTooltipModule,
-    ],
-    providers: [AuthService],
-    bootstrap: [],
+  declarations: [DashboardComponent, MyProfileComponent],
+  imports: [
+    DashbaordRoutingModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    CommonModule,
+    ColorPickerModule,
+    FormsModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
+  providers: [AuthService],
+  bootstrap: [],
 })
 export class DashbaordModule {}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,151 +7,151 @@ import { AuthService } from "src/app/Services/auth.service";
 import { ColorCode } from "src/app/shared/colorCode";
 
 @Component({
-    selector: "app-dashboard",
-    templateUrl: "./dashboard.component.html",
-    styleUrls: ["./dashboard.component.css"],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
-    title = "angular-firebase";
-    loading: boolean = false;
-    isEdit: boolean = false;
-    user_uuid!: string | null;
+  title = "angular-firebase";
+  loading: boolean = false;
+  isEdit: boolean = false;
+  user_uuid!: string | null;
 
-    color: any[] = [];
-    selectedColor!: any;
+  color: any[] = [];
+  selectedColor!: any;
 
-    email!: string | null;
-    form!: FormGroup;
-    hoveredIndex!: number | null;
+  email!: string | null;
+  form!: FormGroup;
+  hoveredIndex!: number | null;
 
-    noteName!: FormControl;
-    noteDescription!: FormControl;
+  noteName!: FormControl;
+  noteDescription!: FormControl;
 
-    dataSource: any;
-    id: any;
-    name: any;
-    personalInfo: any;
-    editObj: any;
+  dataSource: any;
+  id: any;
+  name: any;
+  personalInfo: any;
+  editObj: any;
 
-    @ViewChild("btnShow")
-    btnShow!: ElementRef;
-    @ViewChild("btnClose")
-    btnClose!: ElementRef;
+  @ViewChild("btnShow")
+  btnShow!: ElementRef;
+  @ViewChild("btnClose")
+  btnClose!: ElementRef;
 
-    constructor(
-        private store: AngularFirestore,
-        private spinner: NgxSpinnerService,
-        public auth: AuthService,
-        private router: Router,
-        private fb: FormBuilder
-    ) {
-        this.noteName = new FormControl("", [Validators.required]);
-        this.noteDescription = new FormControl("", [Validators.required]);
+  constructor(
+    private store: AngularFirestore,
+    private spinner: NgxSpinnerService,
+    public auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.noteName = new FormControl("", [Validators.required, Validators.maxLength(30)]);
+    this.noteDescription = new FormControl("", [Validators.required]);
 
-        this.form = fb.group({
-            noteName: this.noteName,
-            noteDescription: this.noteDescription,
+    this.form = fb.group({
+      noteName: this.noteName,
+      noteDescription: this.noteDescription,
+    });
+  }
+
+  ngOnInit() {
+    var user = JSON.parse(localStorage.getItem("user") || "{}");
+    this.user_uuid = user.uid;
+    this.email = user.email;
+    this.getAll();
+    this.color = ColorCode;
+    console.log(this.color);
+    this.selectedColor = "#000";
+  }
+
+  navigateToProfile() {
+    this.router.navigate(["/myprofile"]);
+  }
+
+  openDialog() {
+    this.btnShow.nativeElement.click();
+  }
+
+  closeDialog() {
+    this.btnClose.nativeElement.click();
+  }
+
+  clearEdit() {
+    this.editObj = null;
+    this.name = "";
+    this.personalInfo = "";
+    this.selectedColor = "#000";
+  }
+
+  add() {
+    this.spinner.show();
+    if (this.editObj) {
+      this.store
+        .collection("list")
+        .doc("users")
+        .collection(`${this.user_uuid}`)
+        .doc(this.editObj.id)
+        .update({ name: this.name, personalInfo: this.personalInfo, colorCode: this.selectedColor });
+      this.spinner.hide();
+      this.clearEdit();
+      this.isEdit = false;
+    } else {
+      this.store
+        .collection("list")
+        .doc("users")
+        .collection(`${this.user_uuid}`)
+        .add({ name: this.name, personalInfo: this.personalInfo, colorCode: this.selectedColor });
+      this.spinner.hide();
+      this.clearEdit();
+    }
+    this.closeDialog();
+  }
+
+  edit(id: string) {
+    this.isEdit = true;
+    this.store
+      .collection("list")
+      .doc("users")
+      .collection(`${this.user_uuid}`)
+      .doc(id)
+      .get()
+      .subscribe((response) => {
+        this.editObj = Object.assign({ id: response.id }, response.data());
+        this.name = this.editObj.name;
+        this.personalInfo = this.editObj.personalInfo;
+        this.selectedColor = this.editObj.colorCode;
+        this.openDialog();
+      });
+  }
+
+  delete(id: string) {
+    this.store.collection("list").doc("users").collection(`${this.user_uuid}`).doc(id).delete();
+  }
+
+  getAll() {
+    this.spinner.show();
+    setTimeout(() => {
+      this.store
+        .collection("list")
+        .doc("users")
+        .collection(`${this.user_uuid}`)
+        .snapshotChanges()
+        .subscribe((response) => {
+          this.dataSource = response.map((item) => {
+            return Object.assign({ id: item.payload.doc.id }, item.payload.doc.data());
+          });
+          console.log(this.dataSource);
         });
-    }
+      this.spinner.hide();
+      this.loading = true;
+    }, 1500);
+  }
 
-    ngOnInit() {
-        var user = JSON.parse(localStorage.getItem("user") || "{}");
-        this.user_uuid = user.uid;
-        this.email = user.email;
-        this.getAll();
-        this.color = ColorCode;
-        console.log(this.color);
-        this.selectedColor = "#000";
+  get isAddUpdate() {
+    if (this.noteName.valid && this.noteDescription.valid && this.selectedColor) {
+      return false;
+    } else {
+      return true;
     }
-
-    navigateToProfile() {
-        this.router.navigate(["/myprofile"]);
-    }
-
-    openDialog() {
-        this.btnShow.nativeElement.click();
-    }
-
-    closeDialog() {
-        this.btnClose.nativeElement.click();
-    }
-
-    clearEdit() {
-        this.editObj = null;
-        this.name = "";
-        this.personalInfo = "";
-        this.selectedColor = "#000";
-    }
-
-    add() {
-        this.spinner.show();
-        if (this.editObj) {
-            this.store
-                .collection("list")
-                .doc("users")
-                .collection(`${this.user_uuid}`)
-                .doc(this.editObj.id)
-                .update({ name: this.name, personalInfo: this.personalInfo, colorCode: this.selectedColor });
-            this.spinner.hide();
-            this.clearEdit();
-            this.isEdit = false;
-        } else {
-            this.store
-                .collection("list")
-                .doc("users")
-                .collection(`${this.user_uuid}`)
-                .add({ name: this.name, personalInfo: this.personalInfo, colorCode: this.selectedColor });
-            this.spinner.hide();
-            this.clearEdit();
-        }
-        this.closeDialog();
-    }
-
-    edit(id: string) {
-        this.isEdit = true;
-        this.store
-            .collection("list")
-            .doc("users")
-            .collection(`${this.user_uuid}`)
-            .doc(id)
-            .get()
-            .subscribe((response) => {
-                this.editObj = Object.assign({ id: response.id }, response.data());
-                this.name = this.editObj.name;
-                this.personalInfo = this.editObj.personalInfo;
-                this.selectedColor = this.editObj.colorCode;
-                this.openDialog();
-            });
-    }
-
-    delete(id: string) {
-        this.store.collection("list").doc("users").collection(`${this.user_uuid}`).doc(id).delete();
-    }
-
-    getAll() {
-        this.spinner.show();
-        setTimeout(() => {
-            this.store
-                .collection("list")
-                .doc("users")
-                .collection(`${this.user_uuid}`)
-                .snapshotChanges()
-                .subscribe((response) => {
-                    this.dataSource = response.map((item) => {
-                        return Object.assign({ id: item.payload.doc.id }, item.payload.doc.data());
-                    });
-                    console.log(this.dataSource);
-                });
-            this.spinner.hide();
-            this.loading = true;
-        }, 1500);
-    }
-
-    get isAddUpdate() {
-        if (this.noteName.valid && this.noteDescription.valid && this.selectedColor) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+  }
 }
